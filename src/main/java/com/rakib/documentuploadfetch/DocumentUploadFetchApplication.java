ab1cb8b821aa2fc7a39c26c98983388d24b06b12
rakib <rakibccj@gmail.com>
2022-05-21 16:03:22 +0600

    project initiate

---
+++ src/main/java/com/rakib/documentuploadfetch/DocumentUploadFetchApplication.java
@@ -0,0 +1,13 @@
+package com.rakib.documentuploadfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentUploadFetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentUploadFetchApplication.class, args);
+	}
+
+}
